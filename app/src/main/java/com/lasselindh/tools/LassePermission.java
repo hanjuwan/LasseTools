2d9be77bf7400c07797a18517fe81bb7ce72b4c7
juwanhan <hanjuwan@gmail.com>
2018-08-09 13:22:02 +0900

    add getUsageStatsPermission

--- app/src/main/java/com/lasselindh/tools/LassePermission.java
+++ app/src/main/java/com/lasselindh/tools/LassePermission.java
@@ -1,8 +1,10 @@
 package com.lasselindh.tools;
 
 import android.annotation.TargetApi;
+import android.app.AppOpsManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
@@ -17,7 +19,12 @@ public class LassePermission extends AppCompatActivity {
     public static final int DENIED = 0;
     public static final int GRANTED = 1;
     public static final int ALWAYS_DENIED = -1;
+
     private static final int REQUEST_OVERLAY = 1000;
+    private static final int REQUEST_USAGE_STATS = 2000;
+    private static final int REQUEST_PERMISSION = 3000;
+
+    private String[] mPermissions;
     private static PermissionListener mPermissionListener;
     public interface PermissionListener {
         void onRequestResult(boolean allOk, ArrayList<String> denided);
@@ -28,25 +35,39 @@ public class LassePermission extends AppCompatActivity {
         void onCheckCompleted(boolean result);
     }
 
-    private String[] mPermissions;
+    private static UsageStatsListener mUsageStatsListener;
+    public interface UsageStatsListener {
+        void onCheckCompleted(boolean result);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         overridePendingTransition(0, 0);
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
-        if(getIntent().getBooleanExtra("overlay", false)) {
-            if(canDrawOverlays(this)) {
-                mOverlayListener.onCheckCompleted(true);
-                finish();
-            } else {
-                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
-                startActivityForResult(intent, REQUEST_OVERLAY);
-            }
-        } else {
-            CheckPermissionApp(getIntent().getStringArrayExtra("permission"));
-        }
 
+        switch (getIntent().getIntExtra("request", 0)) {
+            case REQUEST_OVERLAY: {
+                if (canDrawOverlays(this)) {
+                    mOverlayListener.onCheckCompleted(true);
+                    finish();
+                } else {
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                    startActivityForResult(intent, REQUEST_OVERLAY);
+                }
+                break;
+            }
+
+            case REQUEST_PERMISSION: {
+                CheckPermissionApp(getIntent().getStringArrayExtra("permission"));
+                break;
+            }
+
+            case REQUEST_USAGE_STATS: {
+                startActivityForResult(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS), REQUEST_USAGE_STATS);
+                break;
+            }
+        }
     }
 
     @Override
@@ -65,6 +86,13 @@ public class LassePermission extends AppCompatActivity {
                 break;
             }
 
+            case REQUEST_USAGE_STATS: {
+                if(checkUsageStatsPermission(this)) {
+                    mUsageStatsListener.onCheckCompleted(true);
+                } else {
+                    mUsageStatsListener.onCheckCompleted(false);
+                }
+            }
         }
     }
 
@@ -140,7 +168,7 @@ public class LassePermission extends AppCompatActivity {
             mPermissionListener.onRequestResult(true, null);
         } else {
             Intent intent = new Intent(context, LassePermission.class);
-            intent.putExtra("overlay", false);
+            intent.putExtra("request", REQUEST_PERMISSION);
             intent.putExtra("permission", permissions);
             context.startActivity(intent);
         }
@@ -157,9 +185,38 @@ public class LassePermission extends AppCompatActivity {
                 mOverlayListener.onCheckCompleted(true);
             } else {
                 Intent intent = new Intent(context, LassePermission.class);
-                intent.putExtra("overlay", true);
+                intent.putExtra("request", REQUEST_OVERLAY);
                 context.startActivity(intent);
             }
         }
     }
+
+    public static void getUsageStats(Context context, UsageStatsListener listener) {
+        mUsageStatsListener = listener;
+
+        if(checkUsageStatsPermission(context)) {
+            mUsageStatsListener.onCheckCompleted(true);
+        } else {
+            Intent intent = new Intent(context, LassePermission.class);
+            intent.putExtra("request", REQUEST_USAGE_STATS);
+            context.startActivity(intent);
+        }
+    }
+
+    private static boolean checkUsageStatsPermission(Context context) {
+        if(Build.VERSION.SDK_INT >= 19) {
+            try {
+                PackageManager packageManager = context.getPackageManager();
+                ApplicationInfo applicationInfo = packageManager.getApplicationInfo(context.getPackageName(), 0);
+                AppOpsManager appOpsManager = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+                int mode = appOpsManager.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, applicationInfo.uid, applicationInfo.packageName);
+                return (mode == AppOpsManager.MODE_ALLOWED);
+
+            } catch (PackageManager.NameNotFoundException e) {
+                return false;
+            }
+        }
+        return false;
+    }
+
 }
